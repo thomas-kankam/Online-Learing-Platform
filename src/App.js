da148eb7df901ff9d31952edb38f1c2e82f9795f
Paa Tommy <kankamthomas6@gmail.com>
2021-07-03 18:45:02 -0700

    An online learning platform
    This a complete learning platform built using react framework.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './css/props.css';
+import './css/App.css';
+
+// Screens
+import Header from './screens/header';
+import Sidebar from './screens/sidebar';
+
+function App() {
+  return (
+    <div className="App flex">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../ui/logo1.png'
+
+function Header(){
+    return(
+        <div className="header fixed">
+        </div>
+    )
+}
+
+export default Header;
